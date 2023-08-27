1b1c5535e122c17fd8f7cdc186afae4745a0aae0
the-soban <sobanshafeq123@gmail.com>
2023-08-27 16:50:59 +0500

    created and styled navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <div className="navbar">
+            <h2 className="logo">
+                chatter<span className="dot">.</span>
+            </h2>
+            <div className="user">
+                <img
+                    src="https://images.unsplash.com/photo-1593283590172-adfce2adf213?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80"
+                    alt=""
+                />
+                <span className="name">Soban</span>
+                <button>Logout</button>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
+    return (
+        <div className="sidebar">
+            <Navbar />
+        </div>
+    )
 }
 
 export default Sidebar
