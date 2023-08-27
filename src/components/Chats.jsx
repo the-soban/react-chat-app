154fef40fdc86777b94be33375124259058cbb73
the-soban <sobanshafeq123@gmail.com>
2023-08-27 17:37:43 +0500

    created, styled search-bar and chats section

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 
 const Chats = () => {
-  return (
-    <div>Chats</div>
-  )
+    return (
+        <div className="chats">
+            <div className="user-chat">
+                <img
+                    src="https://images.unsplash.com/photo-1593283590172-adfce2adf213?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80"
+                    alt=""
+                />
+                <div className="user-chat-info">
+                    <span>Soban</span>
+                    <p>Hello mate!</p>
+                </div>
+            </div>
+
+            <div className="user-chat">
+                <img
+                    src="https://images.unsplash.com/photo-1593283590172-adfce2adf213?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80"
+                    alt=""
+                />
+                <div className="user-chat-info">
+                    <span>Soban</span>
+                    <p>Hello mate!</p>
+                </div>
+            </div>
+
+            <div className="user-chat">
+                <img
+                    src="https://images.unsplash.com/photo-1593283590172-adfce2adf213?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80"
+                    alt=""
+                />
+                <div className="user-chat-info">
+                    <span>Soban</span>
+                    <p>Hello mate!</p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Chats
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
 const Search = () => {
-  return (
-    <div>Search</div>
-  )
+    return (
+        <div className="search">
+            <div className="search-form">
+                <input type="text" placeholder="Search for a chat..." />
+            </div>
+            <div className="user-chat">
+                <img
+                    src="https://images.unsplash.com/photo-1593283590172-adfce2adf213?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80"
+                    alt=""
+                />
+                <div className="user-chat-info">
+                    <span>Soban</span>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Search
