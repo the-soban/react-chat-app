8bede6d5973e4ab31ceafcfad9be7144240e9bba
the-soban <sobanshafeq123@gmail.com>
2023-08-29 16:11:32 +0500

    styled bottom input field in chatbox

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import AttachIcon from '../images/attach.png'
+import ImgIcon from '../images/img.png'
 
 const Input = () => {
-  return (
-    <div>Input</div>
-  )
+    return (
+        <div className="input">
+            <input type="text" placeholder="Type your msessage..." />
+            <div className="send">
+                <img src={AttachIcon} alt="" />
+                <input type="file" id="file" />
+                <label htmlFor="file">
+                    <img src={ImgIcon} alt="" />
+                </label>
+                <button>Send</button>
+            </div>
+        </div>
+    )
 }
 
 export default Input
