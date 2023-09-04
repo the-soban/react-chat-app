24e87692f2da56450542674c48cbccf1d7c2a368
the-soban <sobanshafeq123@gmail.com>
2023-09-04 23:16:35 +0500

    setup react router and added navigation using useNavigate hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
-import Register from './pages/Register'
-import Login from './pages/Login'
-import Home from './pages/Home'
-import './styles.scss'
+import React from "react";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import "./styles.scss";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
     return (
-        <div>
-            <Register />
-        </div>
-    )
+        <BrowserRouter>
+            <Routes>
+                <Route path="/">
+                    <Route index element={<Home />} />
+                    <Route path="register" element={<Register />} />
+                    <Route path="login" element={<Login />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,9 +4,11 @@ import { auth, db, storage } from "../firebase";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
     const [error, setError] = useState(false);
+    const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -51,8 +53,9 @@ const Register = () => {
                             await setDoc(
                                 doc(db, "userChats", res.user.uid),
                                 {}
-                            );
+                             );
                             console.log("userChats collection created");
+                            navigate("/");
                         }
                     );
                 }
