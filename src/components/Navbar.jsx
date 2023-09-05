b186858b85a038cda5e25bb866391ace9fb3e925
the-soban <sobanshafeq123@gmail.com>
2023-09-05 18:01:39 +0500

    added logic to show current user's name and image in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,19 @@
+import React, { useContext } from "react";
 import { signOut } from "firebase/auth";
-import React from "react";
 import { auth } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 
 const Navbar = () => {
+    const { currentUser } = useContext(AuthContext);
+
     return (
         <div className="navbar">
             <h2 className="logo">
                 chatter<span className="dot">.</span>
             </h2>
             <div className="user">
-                <img
-                    src="https://images.unsplash.com/photo-1593283590172-adfce2adf213?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80"
-                    alt=""
-                />
-                <span className="name">Soban</span>
+                <img src={currentUser.photoURL} alt="" />
+                <span className="name">{currentUser.displayName}</span>
                 <button onClick={() => signOut(auth)}>Logout</button>
             </div>
         </div>
