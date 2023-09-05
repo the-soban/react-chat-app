48761f5ee6e92ca717f328c6d5de9142104e6fb2
the-soban <sobanshafeq123@gmail.com>
2023-09-05 17:31:18 +0500

    added a protected route on home page, prevents unauthurized user's login

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,30 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import "./styles.scss";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
 import { AuthContext } from "./context/AuthContext";
 
 function App() {
     const { currentUser } = useContext(AuthContext);
-    console.log(currentUser);
+
+    const ProtectedRoute = ({ children }) => {
+        if (!currentUser) {
+            return <Navigate to="/login" />;
+        }
+    };
 
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/">
-                    <Route index element={<Home />} />
+                    <Route
+                        index
+                        element={
+                            <ProtectedRoute>
+                                <Home />
+                            </ProtectedRoute>
+                        }
+                    />
                     <Route path="register" element={<Register />} />
                     <Route path="login" element={<Login />} />
                 </Route>
